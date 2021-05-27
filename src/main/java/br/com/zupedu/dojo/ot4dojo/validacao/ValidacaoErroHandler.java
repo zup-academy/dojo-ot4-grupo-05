4a37e85420292e3de48387a2208b2a7ec6fd5ecc
= <jose.souza@zup.com.br>
2021-05-27 16:11:55 -0300

    Priscila

--- src/main/java/br/com/zupedu/dojo/ot4dojo/validacao/ValidacaoErroHandler.java
+++ src/main/java/br/com/zupedu/dojo/ot4dojo/validacao/ValidacaoErroHandler.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -21,6 +23,11 @@ public class ValidacaoErroHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({MethodArgumentNotValidException.class})
     public List<ValidacaoErro> handler(MethodArgumentNotValidException exception) {
-        //List<FieldError> fieldErrors = exception.
+        
+    	List<ValidacaoErro> erros = new ArrayList<>();
+    	
+    	List<FieldError> fileErrors = exception.getBindingResult().getFieldErrors();
+    	
+    	List<ObjectError> objecErrors;
     }
 }
